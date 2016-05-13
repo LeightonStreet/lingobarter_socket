63faf98b4ed7e99d372c38316c28ee8b58fb08d7
Xin-Yang15 <coco.xin.yang2015@gmail.com>
2016-05-13 03:41:24 -0400

    add all emit events for websocket

--- src/main/java/com/lingobarter/socketclient/WebsocketClient.java
+++ src/main/java/com/lingobarter/socketclient/WebsocketClient.java
@@ -5,6 +5,7 @@ import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 import org.json.JSONObject;
 
+import java.io.*;
 import java.net.URISyntaxException;
 
 /**
@@ -25,6 +26,90 @@ public class WebsocketClient {
         JSONObject object = new JSONObject();
         object.put("to_id", to_id);
         this.emit("request new partner", object);
+    }
+
+    public void addPartner(String from_id) {
+        JSONObject object = new JSONObject();
+        object.put("from_id", from_id);
+        this.emit("add partner", object);
+    }
+
+    public void rejectPartner(String from_id) {
+        JSONObject object = new JSONObject();
+        object.put("from_id", from_id);
+        this.emit("reject partner", object);
+    }
+
+    public void browseRequests() {
+        this.emit("browse requests");
+    }
+
+    public void browsePartners() {
+        this.emit("browse partners");
+    }
+
+    public void browseChats() {
+        this.emit("browse chats");
+    }
+
+    public void sendTextMessage(String payload, String to_chat) {
+        JSONObject object = new JSONObject();
+        object.put("payload", payload);
+        object.put("to_chat", to_chat);
+        object.put("type", "text");
+        this.emit("send message", object);
+    }
+
+    public void sendVoiceMessage(String file_path, String to_chat) throws IOException {
+        JSONObject object = new JSONObject();
+        object.put("to_chat", to_chat);
+        object.put("type", "voice");
+
+        FileInputStream fileInputStream = new FileInputStream(file_path);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
+        byte[] data = new byte[4096];
+        int count = fileInputStream.read(data);
+        while (count != -1) {
+            dataOutputStream.write(data, 0, count);
+            count = fileInputStream.read(data);
+        }
+        byte[] payload = byteArrayOutputStream.toByteArray();
+        object.put("payload", payload);
+
+        this.emit("send message", object);
+    }
+
+    public void sendImageMessage(String file_path, String to_chat) throws IOException {
+        JSONObject object = new JSONObject();
+        object.put("to_chat", to_chat);
+        object.put("type", "image");
+
+        FileInputStream fileInputStream = new FileInputStream(file_path);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
+        byte[] data = new byte[4096];
+        int count = fileInputStream.read(data);
+        while (count != -1) {
+            dataOutputStream.write(data, 0, count);
+            count = fileInputStream.read(data);
+        }
+        byte[] payload = byteArrayOutputStream.toByteArray();
+        object.put("payload", payload);
+
+        this.emit("send message", object);
+    }
+
+    public void browseMessages(String page_size, String page_id, String to_chat) {
+        JSONObject object = new JSONObject();
+        object.put("page_size", page_size);
+        object.put("page_id", page_id);
+        object.put("to_chat", to_chat);
+        this.emit("browse messages", object);
+    }
+
+    public void fetchUndeliveredMessages() {
+        this.emit("browse chats");
     }
 
     public void connect() {
---
+++ src/test/java/TestSocketClient2.java
@@ -0,0 +1,31 @@
+import com.lingobarter.socketclient.WebsocketClient;
+import io.socket.emitter.Emitter;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+/**
+ * Created by lihe on 5/13/16.
+ * @author He Li
+ */
+// user1 WyI1NzM1N2E5YjY3YjYwYjEwODNiMDk2MzEiLCIxMzVjZTIwM2ZkMTkyY2JiZTUyNzBmMTQ5NmQ3YWY1MSJd.ChcM2g.4tz9i6M9AtqOAqY7H0FDXoVKz7U
+// user2 WyI1NzM1N2E5YjY3YjYwYjEwODNiMDk2MzIiLCI2MmFjZGY1OTYxNDE5ZDY3N2Q1NjAyM2FlOTAwYjhhZSJd.ChcMgA.7x4kQijpr_GIYNoGtxBuUGpDSqU
+public class TestSocketClient2 {
+    public static void main(String[] args) throws URISyntaxException {
+        final WebsocketClient client = new WebsocketClient("http://192.168.0.4:8080", "WyI1NzM1N2E5YjY3YjYwYjEwODNiMDk2MzEiLCIxMzVjZTIwM2ZkMTkyY2JiZTUyNzBmMTQ5NmQ3YWY1MSJd.ChcM2g.4tz9i6M9AtqOAqY7H0FDXoVKz7U", true);
+
+        JSONObject object = new JSONObject();
+        object.put("to_id", "57357a9b67b60b1083b09632");
+        client.emit("request new partner", object);
+
+        client.on("ret:request new partner", new Emitter.Listener() {
+            public void call(Object... args) {
+                JSONObject ret = (JSONObject)args[0];
+                System.out.println(ret);
+                client.emit("request new partner", "57357a9b67b60b1083b09699");
+            }
+        });
+
+        client.connect();
+    }
+}
